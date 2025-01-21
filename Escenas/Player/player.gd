75a06a3ccec4364d246d5e07de03cf42c1b3cf71
kevinMG01 <montanokevin46@gmail.com>
2025-01-21 10:51:05 -0300

    w

--- Escenas/Player/player.gd
+++ Escenas/Player/player.gd
@@ -10,15 +10,23 @@ var gravity = 2000
 
 var cantidadBurbujas = 20
 
+var colliding_ladder = false
+var going_up = false
+var acceleration = 100
+var max_speed = 200
+
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		if cantidadBurbujas > 0:
 			spawnBurbujas()
 			cantidadBurbujas -= 1
 
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if !going_up:
+		if not is_on_floor():
+			velocity.y += gravity * delta
+			
 
+	climb()
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
@@ -47,5 +55,34 @@ func spawnBurbujas():
 
 
 
+func subirEscaleras():
+	
+	
+	pass
 
 
+func climb():
+	if colliding_ladder:
+		if Input.is_action_pressed("ui_up"):
+			going_up = true
+			velocity.y = max(velocity.y - acceleration, -max_speed)
+		elif Input.is_action_pressed("ui_down"):
+			going_up = true
+			velocity.y = min(velocity.y + acceleration, max_speed)
+		else:
+			if going_up:
+				velocity.y = 0
+func _on_detector_escaleras_area_entered(area):
+	area.get_name()
+	if area.is_in_group("escalera"):
+		colliding_ladder = true
+		
+	pass # Replace with function body.
+
+
+func _on_detector_escaleras_area_exited(area):
+	area.get_name()
+	if area.is_in_group("escalera"):
+		colliding_ladder = false
+		going_up = false
+	pass # Replace with function body.
