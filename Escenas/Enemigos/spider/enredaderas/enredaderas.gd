8191af4eae29508127cfd9fa0af45d0511c243d6
kevinMG01 <montanokevin46@gmail.com>
2025-01-22 19:03:31 -0300

    s

--- Escenas/Enemigos/spider/enredaderas/enredaderas.gd
+++ Escenas/Enemigos/spider/enredaderas/enredaderas.gd
@@ -1,14 +1,26 @@
 extends CharacterBody2D
 
+
+
+
+
 var direccion :Vector2
 @export var velocidad:int
 
 
-func _ready():
-	pass
 
 
 
 func _physics_process(delta):
 	velocity = direccion.normalized() * velocidad
 	move_and_slide()
+
+
+func _on_detector_body_entered(body):
+	direccion = Vector2(0, 0)
+	
+
+
+
+
+
