9434e8bcbc1f28272dbde94a7a8c922cb27f21ea
kevinMG01 <montanokevin46@gmail.com>
2025-01-24 16:04:58 -0300

    areglos

--- Escenas/Menu/Menu.gd
+++ Escenas/Menu/Menu.gd
@@ -1,12 +1,29 @@
 extends Node2D
 
 
+var boton = 0
 
-
-
-func _on_jugar_pressed():
-	pass # Replace with function body.
 
 
 func _on_salir_pressed():
-	pass # Replace with function body.
+	boton = 2
+	$explocion.play()
+	$tiempoSonido.start()
+	$salir.queue_free()
+
+func _on_play_pressed():
+	boton = 1
+	$explocion.play()
+	$tiempoSonido.start()
+	$Play.queue_free()
+
+
+
+
+
+func _on_tiempo_sonido_timeout():
+	if boton == 1:
+		get_tree().change_scene_to_file("res://Escenas/Niveles/nivel_1.tscn")
+	elif boton == 2:
+		get_tree().quit()
+	$tiempoSonido.stop()
